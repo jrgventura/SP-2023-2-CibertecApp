75468b1756bdff626947212bd38301e4024e37d9
jrgventura <jorge.venturag@outlook.com>
2023-09-18 16:52:28 -0500

    semana 05

--- app/src/main/java/com/cibertec/cibertecapp/NoticiasAdapter.kt
+++ app/src/main/java/com/cibertec/cibertecapp/NoticiasAdapter.kt
@@ -1,4 +1,25 @@
 package com.cibertec.cibertecapp
 
-class NoticiasAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class NoticiasAdapter(val list: List<Noticia>):
+    RecyclerView.Adapter<NoticiasViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int):
+            NoticiasViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        return NoticiasViewHolder(inflater, parent)
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    override fun onBindViewHolder(holder: NoticiasViewHolder, position: Int) {
+        val noticia: Noticia = list[position]
+        holder.bind(noticia)
+    }
+
+
 }
--- app/src/main/java/com/cibertec/cibertecapp/WelcomeActivity.kt
+++ app/src/main/java/com/cibertec/cibertecapp/WelcomeActivity.kt
@@ -2,12 +2,26 @@ package com.cibertec.cibertecapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
 
 class WelcomeActivity: AppCompatActivity() {
+
+    private val listNoticias = listOf(
+        Noticia("¿PORQUÉ INVERTIR EN UNA ESPECIALIZACIÓN?",
+            "Una especialización se refiere a la adquisición de conocimientos, habilidades y experiencia más detallados y profundos en un área específica de estudio o práctica",
+            R.drawable.noticia1),
+        Noticia("¿POR QUÉ DEBES ESTUDIAR COMPUTACIÓN E INFORMÁTICA?\n", "La pandemia no solo cambió la forma de trabajo en las empresas, sino también aceleró su digitalización. A consecuencia de ello, la demanda de perfiles tecnológicos se incrementó, siendo el más requerido las personas dedicadas a la Computación e Informática.\n" +
+                "\n", R.drawable.noticia2),
+        Noticia("¿DE QUÉ TRATA LA CARRERA DE ANIMACIÓN DIGITAL?\n", "En la carrera animación digital todos tenemos acceso ilimitado para crear arte cuando y como queramos con la tecnología. Los profesionales de la animación digital pueden trabajar para empresas de cine y televisión, agencias de publicidad, la industria de los videojuegos o muchas otras industrias que utilizan las habilidades creativas.\n" +
+                "\n", R.drawable.noticia3)
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+
+        val recyclerNews = findViewById<RecyclerView>(R.id.recyclerNews)
+
     }
 
 
