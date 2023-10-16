ee0850a92d5c61d12a55121feba16871d11553dd
jrgventura <jorge.venturag@outlook.com>
2023-10-16 14:49:01 -0500

    semana 07

---
+++ app/src/main/java/com/cibertec/cibertecapp/sensores/SensoresActivity.kt
@@ -0,0 +1,12 @@
+package com.cibertec.cibertecapp.sensores
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.cibertec.cibertecapp.R
+
+class SensoresActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sensores)
+    }
+}
