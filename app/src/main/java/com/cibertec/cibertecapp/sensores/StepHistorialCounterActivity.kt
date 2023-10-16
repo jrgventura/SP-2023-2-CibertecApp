c17ca8b5551306352bee54c6cdc6923e8e7a4f85
jrgventura <jorge.venturag@outlook.com>
2023-10-16 16:02:25 -0500

    semana 09

---
+++ app/src/main/java/com/cibertec/cibertecapp/sensores/StepHistorialCounterActivity.kt
@@ -0,0 +1,12 @@
+package com.cibertec.cibertecapp.sensores
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.cibertec.cibertecapp.R
+
+class StepHistorialCounterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_step_historial_counter)
+    }
+}
