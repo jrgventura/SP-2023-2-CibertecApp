0e471ee59149aa086ad5e80e6ec7a413eec01a1b
jrgventura <jorge.venturag@outlook.com>
2023-10-16 16:23:55 -0500

    semana 09

--- app/src/main/java/com/cibertec/cibertecapp/sensores/StepHistorialCounterActivity.kt
+++ app/src/main/java/com/cibertec/cibertecapp/sensores/StepHistorialCounterActivity.kt
@@ -1,12 +1,21 @@
 package com.cibertec.cibertecapp.sensores
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.cibertec.cibertecapp.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class StepHistorialCounterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_step_historial_counter)
+
+        val fabCounter = findViewById<FloatingActionButton>(R.id.fabCounter)
+        fabCounter.setOnClickListener {
+            startActivity(Intent(this,
+                StepCounterActivity::class.java))
+        }
+
     }
 }
