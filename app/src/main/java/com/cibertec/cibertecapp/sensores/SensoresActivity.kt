44e2dd0d6a1977ddd06b31c5e087aa729238abdf
jrgventura <jorge.venturag@outlook.com>
2023-10-16 15:22:04 -0500

    semana 09

--- app/src/main/java/com/cibertec/cibertecapp/sensores/SensoresActivity.kt
+++ app/src/main/java/com/cibertec/cibertecapp/sensores/SensoresActivity.kt
@@ -2,6 +2,8 @@ package com.cibertec.cibertecapp.sensores
 
 import android.content.Context
 import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -9,13 +11,19 @@ import android.widget.TextView
 import com.cibertec.cibertecapp.R
 import java.lang.StringBuilder
 
-class SensoresActivity : AppCompatActivity() {
+class SensoresActivity : AppCompatActivity(), SensorEventListener {
     private lateinit var sensorManager: SensorManager
+    private var mLight: Sensor? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sensores)
 
         sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        mLight = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT)
+        mLight?.also {
+            sensorManager.registerListener(this, it,
+                SensorManager.SENSOR_DELAY_NORMAL)
+        }
 
         getSensorList()
     }
@@ -33,5 +41,15 @@ class SensoresActivity : AppCompatActivity() {
 
         val sensorTextView = findViewById<TextView>(R.id.textSensores) as TextView
         sensorTextView.text = sensorText
+    }
+
+    // Proporcionar un objecto con los datos tomados por el sensor
+    override fun onSensorChanged(p0: SensorEvent?) {
+
+    }
+
+    // Proporciona información sensor y su presición
+    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
+
     }
 }
