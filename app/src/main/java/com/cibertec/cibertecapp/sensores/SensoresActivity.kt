c9475cfbff7ef2ae33969d1312c65af09ebcfb4f
jrgventura <jorge.venturag@outlook.com>
2023-10-16 14:54:47 -0500

    semana 09

--- app/src/main/java/com/cibertec/cibertecapp/sensores/SensoresActivity.kt
+++ app/src/main/java/com/cibertec/cibertecapp/sensores/SensoresActivity.kt
@@ -1,12 +1,37 @@
 package com.cibertec.cibertecapp.sensores
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import com.cibertec.cibertecapp.R
+import java.lang.StringBuilder
 
 class SensoresActivity : AppCompatActivity() {
+    private lateinit var sensorManager: SensorManager
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sensores)
+
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+
+        getSensorList()
+    }
+
+    fun getSensorList() {
+        val deviceSensors: List<Sensor> =
+            sensorManager.getSensorList(Sensor.TYPE_ALL)
+
+        val sensorText = StringBuilder()
+        for (currentSensor in deviceSensors) {
+            sensorText.append(currentSensor.name).append(
+                System.getProperty("line.separator")
+            )
+        }
+
+        val sensorTextView = findViewById<TextView>(R.id.textSensores) as TextView
+        sensorTextView.text = sensorText
     }
 }
