31c5908e9931b2436f9b5aa1e7f069a92ff57ef9
jrgventura <jorge.venturag@outlook.com>
2023-10-23 15:55:58 -0500

    semana 10

--- app/src/main/java/com/cibertec/cibertecapp/login/LoginRepository.kt
+++ app/src/main/java/com/cibertec/cibertecapp/login/LoginRepository.kt
@@ -1,4 +1,16 @@
 package com.cibertec.cibertecapp.login
 
+import com.cibertec.cibertecapp.network.ApiService
+import com.cibertec.cibertecapp.network.request.LoginRequest
+import com.cibertec.cibertecapp.network.response.LoginResponse
+import io.reactivex.Single
+
 class LoginRepository {
+
+    private val api = ApiService().apiService
+
+    fun login(email: String, pass: String): Single<LoginResponse>{
+        return api.login(LoginRequest(email, pass))
+    }
+
 }
