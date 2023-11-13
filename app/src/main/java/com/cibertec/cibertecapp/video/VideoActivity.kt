9329b6cdcd3ac4c217801b50e19dfd389966f8ff
jrgventura <jorge.venturag@outlook.com>
2023-11-13 15:20:08 -0500

    semana 13

--- app/src/main/java/com/cibertec/cibertecapp/video/VideoActivity.kt
+++ app/src/main/java/com/cibertec/cibertecapp/video/VideoActivity.kt
@@ -1,12 +1,25 @@
 package com.cibertec.cibertecapp.video
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.MediaController
+import android.widget.VideoView
 import com.cibertec.cibertecapp.R
 
 class VideoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_video)
+
+        // Video desde url
+        val videoView = findViewById<VideoView>(R.id.videoView)
+        val videoUrl = "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4"
+        videoView.setVideoURI(Uri.parse(videoUrl))
+        videoView.start()
+
+        val mediaController = MediaController(this)
+        mediaController.setAnchorView(videoView)
+        videoView.setMediaController(mediaController)
     }
 }
