b99640bdaa0a83a016a430cd0e3c2dc6ee904b56
jrgventura <jorge.venturag@outlook.com>
2023-11-14 20:54:34 -0500

    semana 13

--- app/src/main/java/com/cibertec/cibertecapp/noticias/WelcomeActivity.kt
+++ app/src/main/java/com/cibertec/cibertecapp/noticias/WelcomeActivity.kt
@@ -3,11 +3,15 @@ package com.cibertec.cibertecapp.noticias
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.cibertec.cibertecapp.MenuActivity
 import com.cibertec.cibertecapp.R
+import com.cibertec.cibertecapp.mapas.MapasActivity
+import com.google.android.material.navigation.NavigationView
 
 class WelcomeActivity: AppCompatActivity(), NoticiasAdapter.ItemClickNoticia {
 
@@ -34,6 +38,8 @@ class WelcomeActivity: AppCompatActivity(), NoticiasAdapter.ItemClickNoticia {
             }
 
         }
+
+        configurationDrawer()
     }
 
     override fun onItemClick(noticia: NoticiaFirebase) {
@@ -45,5 +51,36 @@ class WelcomeActivity: AppCompatActivity(), NoticiasAdapter.ItemClickNoticia {
         startActivity(intent)
     }
 
+
+    fun configurationDrawer() {
+
+        val navView = findViewById<NavigationView>(R.id.navigation_view)
+        val drawerLayout = findViewById<DrawerLayout>(R.id.drawerLayout)
+        navView.setNavigationItemSelectedListener { menuItem ->
+
+            drawerLayout.closeDrawer(GravityCompat.START)
+
+            when(menuItem.itemId) {
+                R.id.nav_home -> {
+                    // sdsdsdszdsdsdsd
+                    true
+                }
+                R.id.nav_perfil -> {
+                    // sdsdsdszdsdsdsd
+                    true
+                }
+                R.id.nav_mapas -> {
+                    // sdsdsdszdsdsdsd
+                    startActivity(Intent(this, MapasActivity::class.java))
+                    true
+                }
+                else -> {
+                    false
+                }
+            }
+
+        }
+
+    }
 
 }
