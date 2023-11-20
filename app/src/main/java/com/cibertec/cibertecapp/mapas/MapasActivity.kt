40030b0f66bf6f9ac00a7740e0bb193bec93f937
jrgventura <jorge.venturag@outlook.com>
2023-11-20 15:05:00 -0500

    semana 13

--- app/src/main/java/com/cibertec/cibertecapp/mapas/MapasActivity.kt
+++ app/src/main/java/com/cibertec/cibertecapp/mapas/MapasActivity.kt
@@ -3,10 +3,21 @@ package com.cibertec.cibertecapp.mapas
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.cibertec.cibertecapp.R
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
 
-class MapasActivity : AppCompatActivity() {
+class MapasActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mapas)
+
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.fragmentMap)
+                as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(p0: GoogleMap) {
+
     }
 }
